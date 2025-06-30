bd0907912ba5398c6ca353953cbde4e66992156b
Terry CLombardi <83511726+TerryL1971@users.noreply.github.com>
2025-06-30 13:43:34 +0200

    Update api.js

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
 // if you're using your physical phone, change this to the deployed url
-export const API_URL = "https://recipe-app-njpu.onrender.com/api";
+export const API_URL = "https://localhost:5001//api";
